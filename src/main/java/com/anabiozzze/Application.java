8e5c4db235522f9a8e9ba9549f60d312433c8517
Andrei Pleshachkov <justme7053@gmail.com>
2019-02-27 23:45:07 +0300

    Base structure is ready. Problem with mustache.

---
+++ src/main/java/com/anabiozzze/Application.java
@@ -0,0 +1,17 @@
+package com.anabiozzze;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// аннотация ниже автоматически применяет такие настройки, как @Configuration, @EnableWebMvc, @ComponentScan
+// т.е. может искать бины в этом классе, конфигурации в пакете (контроллер, например), настраивает DispatcherServlet
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
